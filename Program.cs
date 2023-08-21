c6c46dfec99f6d7a37200203395a81d096037e35
John Lu <johnlu2006@gmail.com>
2023-08-21 15:43:59 -0500

    1-4 menu options

--- Program.cs
+++ Program.cs
@@ -19,3 +19,56 @@ List<Product> products = new List<Product>
         new Product("Phoenix Feather Wand", 130.00M, false, 4), // Wands
         new Product("Frog Choir Ensemble", 40.00M, true, 1) // Apparel
     };
+
+
+string greeting = @">>>
+Welcome to ReductioAbsurdum
+Your source for magical supplies";
+
+Console.WriteLine(greeting);
+string choice = null;
+
+while (choice != "0")
+{
+    Console.WriteLine("\nPress any key to enter the main menu...");
+    Console.ReadKey();
+    Console.Clear();
+
+    InitialOptions();
+
+    choice = Console.ReadLine().Trim();
+    switch (choice)
+    {
+        case "0":
+            Console.WriteLine("Goodbye!");
+            break;
+        case "1":
+            //ListProducts();
+            break;
+        case "2":
+            //AddProduct();
+            break;
+        case "3":
+            // Implement the DeleteProduct method
+            // DeleteProduct();
+            break;
+        case "4":
+            // Implement the UpdateProduct method
+            // UpdateProduct();
+            break;
+        default:
+            Console.WriteLine($"Choose a valid option from the menu!");
+            break;
+    }
+}
+
+void InitialOptions()
+{
+    Console.WriteLine(@">>>
+                Choose an option:
+                0. Exit
+                1. View All Products
+                2. Add a Product to Inventory
+                3. Delete a Product from Inventory
+                4. Update Product Details");
+}
