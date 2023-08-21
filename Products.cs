c14a0ed074663bfd9d214d2a36b548d0ed9bc094
John Lu <johnlu2006@gmail.com>
2023-08-21 15:04:32 -0500

    1-1 Products.cs

---
+++ Products.cs
@@ -0,0 +1,37 @@
+public class Product
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public bool IsAvailable { get; set; }
+    public int ProductTypeId { get; set; }
+    
+/* 这是直接用Product method来加Product
+
+    public Product(string name, decimal price, bool isAvailable, int productTypeId)
+    {
+        Name = name;
+        Price = price;
+        IsAvailable = isAvailable;
+        ProductTypeId = productTypeId;
+    } 
+    
+*/
+
+}
+
+/* 
+public class Inventory
+{
+    private List<Product> products = new List<Product>();
+
+    public void AddProduct(Product product)
+    {
+        products.Add(product);
+    }
+
+    public List<Product> GetProductsByType(int productTypeId)
+    {
+        return products.Where(p => p.ProductTypeId == productTypeId).ToList();
+    }
+}
+ */
